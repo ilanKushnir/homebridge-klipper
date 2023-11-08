3681d93ec68cc39d9a8cd9814dbbb86e5a0a55a2
ilanKushnir <ilan@zooz.com>
2023-11-08 21:44:51 +0200

    feat: define components hierarchy

--- src/platformAccessory.ts
+++ src/platformAccessory.ts
@@ -1,13 +1,13 @@
 import { Service, PlatformAccessory, CharacteristicValue } from 'homebridge';
 
-import { ExampleHomebridgePlatform } from './platform';
+import { KlipperPlatform } from './platform';
 
 /**
  * Platform Accessory
  * An instance of this class is created for each accessory your platform registers
  * Each accessory may expose multiple services of different service types.
  */
-export class ExamplePlatformAccessory {
+export class PrinterAccessory {
   private service: Service;
 
   /**
@@ -20,7 +20,7 @@ export class ExamplePlatformAccessory {
   };
 
   constructor(
-    private readonly platform: ExampleHomebridgePlatform,
+    private readonly platform: KlipperPlatform,
     private readonly accessory: PlatformAccessory,
   ) {
 
